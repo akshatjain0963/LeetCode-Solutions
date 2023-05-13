ba79b2779862f4769fff18f730f33e403e709d5c
akshatjain0963 <akshatjain0963@gmail.com>
2023-05-13 08:33:16 +0530

    Added solution - CodeSyncer

--- leetcode/2466.-count-ways-to-build-good-strings/solution.cpp
+++ leetcode/2466.-count-ways-to-build-good-strings/solution.cpp
@@ -13,7 +13,20 @@ public:
         return ans;
     }
     int countGoodStrings(int low, int high, int zero, int one) {
-        vector<ll>dp(high+1,-1);
-      return c(0,low,high,dp,zero,one);   
+        vector<ll>dp(high+1,0);
+        dp[0]=1;
+        ll ans=0;
+        for(int i=1;i<=high;i++){
+            if(i>=zero){
+                dp[i] = (dp[i]%mod + dp[abs(zero-i)]%mod)%mod; 
+            }
+            if(i>=one){
+                dp[i] = (dp[i]%mod + dp[abs(one-i)]%mod)%mod; 
+            }
+            if(i>=low){
+                ans = (ans%mod + dp[i]%mod)%mod;
+            }
+        }
+        return ans;
     }
 };
