c5cb9fef1400e3912daa10d69db163d78638b04d
akshatjain0963 <akshatjain0963@gmail.com>
2023-05-17 22:10:36 +0530

    Added solution - CodeHub

--- gfg/matrix-chain-multiplication/solution.cpp
+++ gfg/matrix-chain-multiplication/solution.cpp
@@ -2,20 +2,9 @@
 // User function Template for C++
 class Solution{
 public:
-    int c(int i,int j,int arr[],vector<vector<int>>&dp){
-        if(i==j)return 0;
-        if(dp[i][j]!=-1)return dp[i][j];
-        int ans=INT_MAX;
-        for(int k=i;k<j;k++){
-            int s=arr[i-1]*arr[k]*arr[j]+c(i,k,arr,dp)+c(k+1,j,arr,dp);
-            ans=min(ans,s);
-        }
-        dp[i][j]=ans;
-        return ans;
-    }
     int matrixMultiplication(int n, int arr[])
     {
-        // code here
+        // code here 
         vector<vector<int>>dp(n,vector<int>(n,0));
         for(int i=n-1;i>=1;i--){
             for(int j=i+1;j<n;j++){
