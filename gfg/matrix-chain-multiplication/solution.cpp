942b1c4cf1dab40aa493aa7221cf6a1bc0964c33
akshatjain0963 <akshatjain0963@gmail.com>
2023-05-17 22:09:37 +0530

    Added solution - CodeHub

--- gfg/matrix-chain-multiplication/solution.cpp
+++ gfg/matrix-chain-multiplication/solution.cpp
@@ -16,8 +16,18 @@ public:
     int matrixMultiplication(int n, int arr[])
     {
         // code here
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        return c(1,n-1,arr,dp);
+        vector<vector<int>>dp(n,vector<int>(n,0));
+        for(int i=n-1;i>=1;i--){
+            for(int j=i+1;j<n;j++){
+                int mx=INT_MAX;
+                for(int k=i;k<j;k++){
+                    int s=arr[i-1]*arr[k]*arr[j]+dp[i][k]+dp[k+1][j];
+                    mx= min(mx,s);
+                }
+                dp[i][j]=mx;
+            }
+        }
+        return dp[1][n-1];
     }
 };
 ...// } Driver Code Ends
